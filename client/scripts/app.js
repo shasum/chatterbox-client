72ab93926c7dd947286f2b43d4b931df87d0cd19
Sanjay Chakrapani <csanjay@gmail.com>
2015-08-31 23:30:39 -0400

    Midway checkin, able to capture user input message

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,25 +1,46 @@
+// Get username
+var userName = window.prompt("Please enter your username");
+
 var $chatFeed = $('.chat');
+var recentMessageId;
 
 var displayMessages = function(data) {
   console.log(data);
   var messages = data.results;
-  for (var i = 0; i < messages.length; i++) {
-    var message = messages[i];
-    var $date = ($.format.date(message.createdAt, 'MMM d h:mm p'));
+  var message;
+  var i;
+
+  // Eliminate previously displayed messages
+  if (recentMessageId !== undefined) {
+    for (i = 0; i < messages.length; i++) {
+      console.log('i = ',i);
+      if (messages[i].objectId === recentMessageId) {
+        messages.splice(0, i+1);
+        break;
+      }
+    }
+  }
+
+  // add messages to document in reverse chronological order
+  for (i = 0; i < messages.length; i++) {
+    message = messages[i];
+    var $date = ($.format.date(message.createdAt, 'MMM d h:mm:ss p'));
     var $newMessage = $('<div class="message"></div>');
     $newMessage.html(
       '<div class="message-date">' + $date +
       '</div><div class="username">' + _.escape(message.username) +
       '</div><div class="message-text">' + _.escape(message.text) + '</div>');
     $newMessage.prependTo($chatFeed);
+    recentMessageId = message.objectId;
   }
 };
+
 
 var errorHandler = function() {
   console.error('error');
 };
 
-// ajax get all check on messages
+// ajax fetch chat messages
 var getMessages = function() {
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
@@ -29,4 +50,13 @@ var getMessages = function() {
   });
 };
 
+// Initialize feed and fetch messages every 1s
 getMessages();
+setInterval(getMessages, 1000);
+
+// Posting message
+$('.submit-btn').on('click', function(event){
+  event.preventDefault();
+  var $textInput = $(this).closest('form').find('textarea');
+  console.log($textInput.val());
+});
