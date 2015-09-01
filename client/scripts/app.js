b7a02c510d227c2115766e1cd55921928c3a0326
Sanjay Chakrapani <csanjay@gmail.com>
2015-08-31 20:12:24 -0400

    Chat messages fetched from server and displayed

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,32 @@
-// YOUR CODE HERE:
+var $chatFeed = $('.chat');
 
+var displayMessages = function(data) {
+  console.log(data);
+  var messages = data.results;
+  for (var i = 0; i < messages.length; i++) {
+    var message = messages[i];
+    var $date = ($.format.date(message.createdAt, 'MMM d h:mm p'));
+    var $newMessage = $('<div class="message"></div>');
+    $newMessage.html(
+      '<div class="message-date">' + $date +
+      '</div><div class="username">' + _.escape(message.username) +
+      '</div><div class="message-text">' + _.escape(message.text) + '</div>');
+    $newMessage.prependTo($chatFeed);
+  }
+};
+
+var errorHandler = function() {
+  console.error('error');
+};
+
+// ajax get all check on messages
+var getMessages = function() {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    success: displayMessages,
+    error: errorHandler
+  });
+};
+
+getMessages();
